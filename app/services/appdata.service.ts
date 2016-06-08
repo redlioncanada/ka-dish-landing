57f76b091da4f4e00e2c0c668ce043fcb1f43c24
Jacoub Bondre <jacoub.bondre@redlioncanada.com>
2016-06-08 11:55:47 -0400

    Updated french link as well.

--- app/services/appdata.service.ts
+++ app/services/appdata.service.ts
@@ -409,7 +409,7 @@ export class AppDataService {
 				title: 'Choisissez votre style',
 				nav: {
 					text: 'Tout voir',
-					link: 'http://www.kitchenaid.ca/fr_CA/shop/major-appliances-1/dishwashers-3/102310014/',
+					link: 'http://www.kitchenaid.ca/fr_CA/magasiner/gros-electromenagers-1/lave-vaisselle-3/102310014/',
 					analytics: {
 						category: "@category",
 						action: "Clicked Choose Your Dish Style",
